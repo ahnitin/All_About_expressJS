2c8e4f3f533e2100faf2f0658c86bf55952c3d23
ahnitin <nitinahuja240@gmail.com>
2023-10-07 00:41:56 +0530

    Filtering Paths

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const shoproutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminroutes);
+app.use("/admin",adminroutes);
 app.use(shoproutes);
 app.use("/",(req,res,next)=>{
     res.status(404).send("<h1>PAGE NOT FOUND<h1>")
