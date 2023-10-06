cc17c135e3db2a9031f0ad252ee28b6dfc8a94aa
ahnitin <nitinahuja240@gmail.com>
2023-10-06 19:22:57 +0530

    the  use of middleWares

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-//third party packages
 const express = require("express");
 const app = express();
-//use allows us to add a new middleWare function
-app.use((req,res,next)=>{
-    console.log("This is the Middleware")
+
+app.use("/",(req,res,next)=>{
+    console.log("this always runs");
     next();
-    // next is a function used to allow the request to move to the next middle ware.
 })
-app.use((req,res,next)=>{
+
+app.use("/add-product",(req, res, next)=>{
+
     console.log("this is the next middle Ware");
-    //.. here we are going to add a response
+    res.send("<h1>The ADD PRODUCT Page</h1>")
+});
+
+app.use("/",(req, res, next)=>{
+
+    console.log("this is the next middle Ware");
     res.send("<h1>Hello from express js</h1>")
-})
+});
 
-
-/* const server = http.createServer(app)
-server.listen(3000); 
-it internally doing the same as it is in the comment section
-*/
-app.listen(3000);
+app.listen(3000); 
