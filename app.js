fe805faa973892f33e289ff178f2e285fe0adb25
ahnitin <nitinahuja240@gmail.com>
2023-10-06 19:44:23 +0530

    Parsing Requests and using Body-parser

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
-app.use("/",(req,res,next)=>{
-    console.log("this always runs");
-    next();
-})
+app.use(bodyParser.urlencoded({extended: false}));
+//before in nodejs we are using chunks and and parsing them and now it's just a 2 lines of code
 
 app.use("/add-product",(req, res, next)=>{
 
-    console.log("this is the next middle Ware");
-    res.send("<h1>The ADD PRODUCT Page</h1>")
+    res.send('<form action="/product" method="POST"><input type="text" name ="title"><button type="submit">ADD Product</button></form>')
 });
+
+app.use("/product",(req,res,next)=>{
+    console.log(req.body.title);
+    res.redirect("/")
+})
 
 app.use("/",(req, res, next)=>{
 
-    console.log("this is the next middle Ware");
     res.send("<h1>Hello from express js</h1>")
 });
 
