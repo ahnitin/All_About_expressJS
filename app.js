7e4b2e59fa024e9f5047dd458af6642e33a83599
ahnitin <nitinahuja240@gmail.com>
2023-10-06 16:08:25 +0530

    Installing dependencies& Basic overview

---
+++ app.js
@@ -0,0 +1,20 @@
+//node specific modules
+const http = require("http");
+//third party packages
+const express = require("express");
+const app = express();
+//use allows us to add a new middleWare function
+app.use((req,res,next)=>{
+    console.log("This is the Middleware")
+    next();
+    // next is a function used to allow the request to move to the next middle ware.
+})
+app.use((req,res,next)=>{
+    console.log("this is the next middle Ware");
+    //.. here we are going to add a response
+    res.send("<h1>Hello from express js</h1>")
+})
+
+
+const server = http.createServer(app)
+server.listen(3000);
