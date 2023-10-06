1a9790b6b2ac97c696428d8d3720054b68411876
ahnitin <nitinahuja240@gmail.com>
2023-10-07 00:26:51 +0530

    using express Router

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+const adminroutes = require("./routes/admin");
+const shoproutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({extended: false}));
 //before in nodejs we are using chunks and and parsing them and now it's just a 2 lines of code
 
-app.use("/add-product",(req, res, next)=>{
 
-    res.send('<form action="/product" method="POST"><input type="text" name ="title"><input type="text" name="size"><button type="submit">ADD Product</button></form>')
-});
-
-app.post("/product",(req,res,next)=>{
-    console.log(req.body);
-    res.redirect("/")
-})
 // this post will only work when user hits on button Add product
-
-app.use("/",(req, res, next)=>{
-
-    res.send("<h1>Hello from express js</h1>")
-});
-
+app.use(adminroutes);
+app.use(shoproutes);
 app.listen(3000); 
