7f9b38121fe83870341bd69dc4cf78b0e4b82073
ahnitin <nitinahuja240@gmail.com>
2023-10-08 23:21:05 +0530

    Serving Html file-connecting with js-file

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/add-product",(req, res, next)=>{
 
-    res.send('<form action="/admin/product" method="POST"><input type="text" name ="title"><input type="text" name="size"><button type="submit">ADD Product</button></form>')
+    res.sendFile(path.join(__dirname, "../","views","add-product.html"))
 });
 
-router.post("/product",(req,res,next)=>{
+router.post("/add-product",(req,res,next)=>{
     console.log(req.body);
     res.redirect("/")
 })
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,15 @@
+const path = require("path")
+// as when we try to set the path by using send file it was not working because we are trying to 
+// attach the file which was not accessible because it started from a root folder.
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/",(req, res, next)=>{
 
-    res.send("<h1>Hello from express js</h1>")
+    res.sendFile(path.join(__dirname,"../","views", "shop.html" ));
+
 });
 
 module.exports = router;
