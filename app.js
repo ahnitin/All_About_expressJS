3059754a224b16cbeabafc8c29664c9fc7cdc00f
ahnitin <nitinahuja240@gmail.com>
2023-10-10 00:42:20 +0530

    Adding Contactus Page

--- app.js
+++ app.js
@@ -6,13 +6,20 @@ const express = require("express");
 const app = express();
 const adminroutes = require("./routes/admin");
 const shoproutes = require("./routes/shop");
-
+const contactroutes = require("./routes/contactus");
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(express.static(path.join(__dirname,"public")))
+//our public css files are not accessible now we need to  so we need to be able to serve files statically 
+// statically means not handled by the express router or our middleware but instead directly forwareded to the
+//file system app.use is a middleware express is an object  and a static method 
 app.use("/admin",adminroutes);
 app.use(shoproutes);
+app.use(contactroutes);
+app.use("/success",(req,res,next)=>{
+    res.status(200).sendFile(path.join(rootDir,"views","success.html"));
+})
 app.use("/",(req,res,next)=>{
     res.status(404).sendFile(path.join(rootDir,"views","404.html"))
 })
-
-app.listen(4020); 
+app.listen(3000); 
---
+++ routes/contactus.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const rootDir= require("../helper/path")
+const router = express.Router();
+
+router.get("/contactus",(req,res,next)=>{
+    res.sendFile(path.join(rootDir,"views","contactus.html"))
+})
+
+router.post("/contactus",(req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/success");
+})
+
+module.exports = router;
