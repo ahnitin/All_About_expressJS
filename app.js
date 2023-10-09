4642eb6662e252ffbb6651342d48f01f355f628e
ahnitin <nitinahuja240@gmail.com>
2023-10-09 10:15:05 +0530

    using helper function to set path

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const bodyParser = require("body-parser");
 const path = require("path");
+const rootDir = require("./helper/path");
+// by using this we don't have to use __dirname and ../ to move to our root directory if we use this it will move to only root directry.
 const express = require("express");
 const app = express();
 const adminroutes = require("./routes/admin");
@@ -10,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use("/admin",adminroutes);
 app.use(shoproutes);
 app.use("/",(req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,"views","404.html"))
+    res.status(404).sendFile(path.join(rootDir,"views","404.html"))
 })
 
 app.listen(3000); 
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,14 @@
 const path = require("path")
 // as when we try to set the path by using send file it was not working because we are trying to 
 // attach the file which was not accessible because it started from a root folder.
-
+const rootDir = require("../helper/path");
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/",(req, res, next)=>{
 
-    res.sendFile(path.join(__dirname,"../","views", "shop.html" ));
+    res.sendFile(path.join(rootDir,"views", "shop.html" ));
 
 });
 
